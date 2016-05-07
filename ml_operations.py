1c4ec86123eec79f5f898c7036cc10d4a8375264
Daniel Temple <dantdj@gmail.com>
2016-05-07 21:16:14 +0100

    Implemented list setup and prototype predictor in ml_operations

--- ml_operations.py
+++ ml_operations.py
@@ -1,6 +1,20 @@
+import csv
 import sklearn
 
 class ml_operations():
 
     def __init__(self):
-        pass
+        with open("features.csv", "rb") as f:
+            reader = csv.reader(f)
+            features = list(reader)
+
+        with open("labels.csv", "rb") as f:
+            reader = csv.reader(f)
+            labels = list(reader)
+
+        classifier = sklearn.tree.DecisionTreeClassifier()
+
+
+    def predict(song_values, adjective):
+        classifier = classifier.fit(features, labels)
+        print classifier.predict([[song_values, adjective]])
--- spotify_operations.py
+++ spotify_operations.py
@@ -7,7 +7,6 @@ class spotify_operations():
 
     def __init__(self):
         spotify = spotipy.Spotify()
-
         spotify_endpoint = "https://api.spotify.com/v1"
 
     def getSongs(userID, playlist, oAuth):
