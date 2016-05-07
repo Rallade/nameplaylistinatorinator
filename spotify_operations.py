39c15bcc32455545fda379fff3f975b7c6f72047
Daniel Temple <dantdj@gmail.com>
2016-05-07 20:23:49 +0100

    Made spotify_operations into a class

---
+++ spotify_operations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import requests
+import spotipy
+
+spotify = spotipy.Spotify()
+
+spotify_endpoint = "https://api.spotify.com/v1/"
+
+def getSongs(userID, playlist, oAuth):
+    tracks = []
+    params = {'playlist'}
+    url = spotify_endpoint + '/users/{}/playlists/{}/tracks'.format(userID, playlist)
