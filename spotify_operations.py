de85bd016d544394d744dea6efe5eb9a8a0de10d
Daniel Temple <dantdj@gmail.com>
2016-05-08 02:51:20 +0100

    Get single track in playlist for now

--- spotify_operations.py
+++ spotify_operations.py
@@ -2,14 +2,25 @@
 
 import requests
 import spotipy
+import json
 
 class spotify_operations():
 
     def __init__(self):
-        spotify = spotipy.Spotify()
-        spotify_endpoint = "https://api.spotify.com/v1"
+        self.spotify = spotipy.Spotify()
+        self.spotify_endpoint = "https://api.spotify.com/v1"
+
+    def get_tracks_in_playlist(self, playlist_json):
+        data = json.load(playlist_json)
+        print(data["items"][0]["track"]["id"])
+
 
     def getSongs(userID, playlist, oAuth):
         tracks = []
         params = {'playlist'}
         url = spotify_endpoint + '/users/{}/playlists/{}/tracks'.format(userID, playlist)
+
+if __name__ == "__main__":
+    with open('playlist.json') as f:
+        so = spotify_operations()
+        so.get_tracks_in_playlist(f)
