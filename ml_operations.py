03e4e15f54ada7176e3827ab4f54c1a95149ed58
Nate Glazier <ng230@sussex.ac.uk>
2016-05-08 08:42:41 +0100

    Fucking. Victory.

--- ml_operations.py
+++ ml_operations.py
@@ -6,25 +6,26 @@ import helpers
 class ml_operations():
 
     def __init__(self):
-        with open("features.csv", "rb") as f:
+        with open("features.csv", "r") as f:
             self.csvreader = csv.reader(f)
             self.features = list(self.csvreader)
 
-        with open("labels.csv", "rb") as f:
+        with open("labels.csv", "r") as f:
             self.csvreader = csv.reader(f)
             self.labels = list(self.csvreader)
 
         i = 0
         self.meta_features = []
         for rows in self.features:
-            self.meta_features.append([self.features[1]])
-            print([self.features[1]])
+            self.meta_features.append([self.features[i][0], self.features[i][1]])
+            print([self.meta_features[i]])
+            i += 1
 
 
     def predict(self):
         classifier = tree.DecisionTreeClassifier()
         classifier = classifier.fit(self.meta_features, self.labels)
-        print classifier.predict([[0.5, 0.6]])
+        print(classifier.predict([0.219, 0.165]))
 
 if __name__ == "__main__":
     ml = ml_operations()
