077bbbdf4a9cb983e7871c28df37c20007e18012
Nate Glazier <ng230@sussex.ac.uk>
2016-05-08 07:46:42 +0100

    prepare for takeoff

--- ml_operations.py
+++ ml_operations.py
@@ -1,16 +1,16 @@
 import csv
 import sklearn
 import helpers
-
+from sklearn import tree
 
 class ml_operations():
 
     def __init__(self):
-        with open("features.csv", "rb") as f:
+        with open("features.csv", "rt") as f:
             reader = csv.reader(f)
             features = list(reader)
 
-        with open("labels.csv", "rb") as f:
+        with open("labels.csv", "rt") as f:
             reader = csv.reader(f)
             labels = list(reader)
 
@@ -21,12 +21,12 @@ class ml_operations():
             meta_features.append([features[1]])
             print([features[1]])
 
-        classifier = sklearn.tree.DecisionTreeClassifier()
 
 
-    def predict(song_values):
+    def predict(self, song_values):
+	classifier = sklearn.tree.DecisionTreeClassifier()
         classifier = classifier.fit(meta_features, labels)
-        print classifier.predict([[song_values]])
+        print(classifier.predict([[song_values]]))
 
 if __name__ == "__main__":
     ml = ml_operations()
