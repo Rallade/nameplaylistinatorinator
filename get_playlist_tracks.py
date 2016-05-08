4cfb60af118403984d9ca34491481c5298a7dc24
Rallade <rallade@hotmail.com>
2016-05-08 01:43:12 +0100

    get_playlist-tracks script
    gets a json with the tracks in a playlist

---
+++ get_playlist_tracks.py
@@ -0,0 +1,29 @@
+import sys
+import urllib
+import json
+import spotipy
+import spotipy.util as util
+
+SPOTIPY_CLIENT_ID='c61ea8f273e341faa04681b289894ee6'
+SPOTIPY_CLIENT_SECRET='83f8d583b70d420186ab8be3c9d9faac'
+SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
+
+scope = 'playlist-modify-private'
+
+if len(sys.argv) > 2:
+    username = sys.argv[1]
+    playlist_id = sys.argv[2]
+else:
+    print("Usage: %s username playlist_id..." % (sys.argv[0],))
+    sys.exit()
+	
+token = util.prompt_for_user_token(username, scope, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI)
+	
+if token:
+	sp = spotipy.Spotify(auth=token)
+	sp.trace = False
+	playlist = sp.user_playlist_tracks(username, '2IEfJNoWklzEaJY1jNTLdE')
+	with open('playlist.json', 'w') as f1:
+		json.dump(playlist, f1)
+else:
+    print("Can't get token for", username)
