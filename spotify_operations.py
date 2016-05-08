f1e0efa6bb9314946fbb3f2f667c689b6f61b67c
Daniel Temple <dantdj@gmail.com>
2016-05-08 06:27:12 +0100

    final commit before first test

--- spotify_operations.py
+++ spotify_operations.py
@@ -29,8 +29,7 @@ class spotify_operations():
     	SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
 
     	scope = 'playlist-modify-private'
-    	token = util.prompt_for_user_token(self.username, scope, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI)
-
+    	token = "BQCb1qwiGoIBWTh6pEPPf5-U92OZEFosUUNinafO0P0VRYU4bKNA6f3u7XPATnE5v_RqnOyfKlnW0blTGaPPfunzJ6ncd88421c6pgp7kWt_y5sFfLpiX3GOf-BPz8MgYB6cl42IJPh-sIKBFy38bHI8HhHTP3O4W-yPq6CW6Zzku383fCuix1JFgdmodBXGkdUfPMRJufKvd7J9IApqNTU2cGGH_-6QMlBn2kvqLY8O8Q7bkOR-6EarPHe69yqgF3z2vfv4xLMUBQRIoUI"
     	if token:
     		sp = spotipy.Spotify(auth=token)
     		ids = [trackid]
@@ -48,7 +47,7 @@ class spotify_operations():
 
         scope = 'playlist-modify-private'
 
-        token = util.prompt_for_user_token(self.username, scope, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI)
+        token = "BQCb1qwiGoIBWTh6pEPPf5-U92OZEFosUUNinafO0P0VRYU4bKNA6f3u7XPATnE5v_RqnOyfKlnW0blTGaPPfunzJ6ncd88421c6pgp7kWt_y5sFfLpiX3GOf-BPz8MgYB6cl42IJPh-sIKBFy38bHI8HhHTP3O4W-yPq6CW6Zzku383fCuix1JFgdmodBXGkdUfPMRJufKvd7J9IApqNTU2cGGH_-6QMlBn2kvqLY8O8Q7bkOR-6EarPHe69yqgF3z2vfv4xLMUBQRIoUI"
 
         if token:
         	sp = spotipy.Spotify(auth=token)
@@ -70,5 +69,9 @@ if __name__ == "__main__":
         so = spotify_operations()
         playlist_tracks = so.get_playlist_tracks()
         track_id_list = so.get_tracks_in_playlist(playlist_tracks)
+        metadata_list = []
         for track in track_id_list:
-            print(so.get_track_metadata(track))
+            metadata_list.append(so.get_track_metadata(track))
+        i = 0
+        for metadata in metadata_list:
+            print(str(metadata[i]["id"]) + ", " + str(metadata[i]["valence"]) + ", " + str(metadata[i]["energy"]))
